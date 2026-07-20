7a699b708133f039b7c83c14fd5216f53930136b
edewusitaiwo <edewusitaiwo@gmail.com>
2026-07-20 02:01:41 +0100

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./util/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,6 +30,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('json spaces', 2);
 
 // 1) GLOBAL MIDDLEWARE
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-origin
+
+app.options('*', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
